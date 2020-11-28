e132f8f46638f70c4de48f14394c9da578b0e5b4
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2020-11-28 13:01:17 +0200

    Add diff_weekdays

--- lib/calendar_ext.ex
+++ lib/calendar_ext.ex
@@ -1,18 +1,29 @@
 defmodule CalendarExt do
-  @moduledoc """
-  Documentation for `CalendarExt`.
-  """
-
-  @doc """
-  Hello world.
+  @doc ~S"""
+  Calculates the week number of week days between `from_date` and `to_date`.
 
   ## Examples
 
-      iex> CalendarExt.hello()
-      :world
+  iex> CalendarExt.diff_weekdays(~D[2020-10-10], ~D[2020-10-20])
+  6
+
+  It will return 0 if the second date is in the past
+
+  iex> CalendarExt.diff_weekdays(~D[2020-10-10], ~D[2020-08-20])
+  0
 
   """
-  def hello do
-    :world
+  def diff_weekdays(%Date{} = from_date, %Date{} = to_date) do
+    saturday = 6
+
+    if Date.diff(to_date, from_date) > 0 do
+      Date.range(to_date, from_date)
+      |> Enum.reject(fn date -> date == to_date end)
+      |> Enum.reduce(0, fn date, acc ->
+        acc + if Date.day_of_week(date) < saturday, do: 1, else: 0
+      end)
+    else
+      0
+    end
   end
 end
--- test/calendar_ext_test.exs
+++ test/calendar_ext_test.exs
@@ -2,7 +2,88 @@ defmodule CalendarExtTest do
   use ExUnit.Case
   doctest CalendarExt
 
-  test "greets the world" do
-    assert CalendarExt.hello() == :world
+  describe "diff_weekdays" do
+    test "on the same day" do
+      day = elem(Date.new(2010, 7, 3), 1)
+
+      assert CalendarExt.diff_weekdays(day, day) == 0
+    end
+
+    test "for one weekday" do
+      from_date = elem(Date.new(2020, 11, 26), 1)
+      to_date = elem(Date.new(2020, 11, 27), 1)
+
+      assert CalendarExt.diff_weekdays(from_date, to_date) == 1
+    end
+
+    test "for one weekend_day" do
+      from_date = elem(Date.new(2020, 11, 28), 1)
+      to_date = elem(Date.new(2020, 11, 29), 1)
+
+      assert CalendarExt.diff_weekdays(from_date, to_date) == 0
+    end
+
+    test "for one week_day and one weekend day" do
+      from_date = elem(Date.new(2020, 11, 29), 1)
+      to_date = elem(Date.new(2020, 11, 30), 1)
+
+      assert CalendarExt.diff_weekdays(from_date, to_date) == 0
+    end
+
+    test "for one week and one weekend day" do
+      from_date = elem(Date.new(2020, 11, 27), 1)
+      to_date = elem(Date.new(2020, 11, 28), 1)
+
+      assert CalendarExt.diff_weekdays(from_date, to_date) == 1
+    end
+
+    test "for two week days spanning a weekend" do
+      from_date = elem(Date.new(2020, 11, 27), 1)
+      to_date = elem(Date.new(2020, 12, 1), 1)
+
+      assert CalendarExt.diff_weekdays(from_date, to_date) == 2
+    end
+
+    test "for a year" do
+      from_date = elem(Date.new(2010, 7, 3), 1)
+      to_date = elem(Date.new(2011, 7, 3), 1)
+
+      assert CalendarExt.diff_weekdays(from_date, to_date) == 260
+    end
+
+    test "spanning over a year" do
+      from_date = elem(Date.new(2019, 12, 28), 1)
+      to_date = elem(Date.new(2020, 1, 13), 1)
+
+      assert CalendarExt.diff_weekdays(from_date, to_date) == 10
+    end
+
+    test "diff_weekdays for a week period returns 4" do
+      from_date = elem(Date.new(2020, 09, 21), 1)
+      to_date = elem(Date.new(2020, 09, 25), 1)
+
+      assert CalendarExt.diff_weekdays(from_date, to_date) == 4
+    end
+
+    test "from monday to saturday" do
+      from_date = elem(Date.new(2020, 09, 21), 1)
+      to_date = elem(Date.new(2020, 09, 26), 1)
+
+      assert CalendarExt.diff_weekdays(from_date, to_date) == 5
+    end
+
+    test "for Sunday and Saturday returns 0" do
+      from_date = elem(Date.new(2020, 09, 19), 1)
+      to_date = elem(Date.new(2020, 09, 21), 1)
+
+      assert CalendarExt.diff_weekdays(from_date, to_date) == 0
+    end
+
+    test "from_date is after to_date returns 0" do
+      from_date = elem(Date.new(2020, 09, 19), 1)
+      to_date = elem(Date.new(2020, 09, 21), 1)
+
+      assert CalendarExt.diff_weekdays(to_date, from_date) == 0
+    end
   end
 end
