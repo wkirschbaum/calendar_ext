c6be764f987376848fe5e10bafed8e0eda31869b
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2020-11-28 12:05:13 +0200

    Add support for elixir 1.7 upwards

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CalendarExt.MixProject do
     [
       app: :calendar_ext,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
