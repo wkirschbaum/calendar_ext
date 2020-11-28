aabd8730a18a204a814877def7bb614caa38089b
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2020-11-28 11:37:59 +0200

    Generate mix package

---
+++ lib/calendar_ext.ex
@@ -0,0 +1,18 @@
+defmodule CalendarExt do
+  @moduledoc """
+  Documentation for `CalendarExt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CalendarExt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/calendar_ext_test.exs
@@ -0,0 +1,8 @@
+defmodule CalendarExtTest do
+  use ExUnit.Case
+  doctest CalendarExt
+
+  test "greets the world" do
+    assert CalendarExt.hello() == :world
+  end
+end
