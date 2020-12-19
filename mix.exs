75d5443f02b84a064c580d30d859bb8664b425cb
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2020-12-19 09:38:53 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CalendarExt.MixProject do
   def project do
     [
       app: :calendar_ext,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       description: description(),
